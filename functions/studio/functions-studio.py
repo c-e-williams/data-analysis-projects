154c2c94f664bce2daadee3c3a06e9177eeb727e
Cathleen <cathleen@MacBook-Pro-2.local>
2024-09-10 15:37:49 -0500

    pandas exercises

--- functions/studio/functions-studio.py
+++ functions/studio/functions-studio.py
@@ -4,8 +4,18 @@
 # b) Within the function, use the 'list' function to split a string into a list of individual characters
 # c) 'reverse' your new list.
 # d) Use 'join' to create the reversed string and return that string from the function.
-# e) Create a variable of type string to test your new function. # f) Use 'print(reverse_characters(my_variable_name))'; to call the function and verify that it correctly reverses the characters in the string.
+# e) Create a variable of type string to test your new function.
+# f) Use 'print(reverse_characters(my_variable_name))'; to call the function and verify that it correctly reverses the characters in the string.
 # g) Use method chaining to reduce the lines of code within the function.
+# def reverse_characters(string_input):
+#     new_list = list(string_input)
+#     reversed_list = new_list.reverse()
+#     "".join(new_list)
+#     reverse_characters="".join(new_list)
+#     return  reverse_characters
+
+# print(reverse_characters('I love the smell of code in the morning.'))
+    
 
 
 
@@ -13,6 +23,22 @@
 # a) Add an if statement to your reverse_characters function to check the typeof the parameter.
 # b - d) If type is ‘string’, return the reversed string as before. If type is ‘number’, convert the parameter to a string, reverse the characters, then convert it back into a number. Return the reversed number.
 # e) Be sure to print the result returned by the function to verify that your code works for both strings and numbers. Do this before moving on to the next steps.
+# 
+# 
+def reverse_characters(string_input): # defining the function called reverse_characters with the string input parameter.
+    if type(string_input)!= str:
+        return("not a string") 
+    elifif: type(string_input)== str
+    new_list = list(string_input)
+    reversed_list = new_list.reverse()
+    "".join(new_list)
+    reverse_characters="".join(new_list)
+    return  reverse_characters
+
+print(reverse_characters(1234))
+
+
+
 
 # 3) Create a new function with one parameter, which is the list we want to change. The function should:
 # a) Define and initialize an empty list.
@@ -27,3 +53,9 @@
 list_test1 = ['apple', 'potato', 'Capitalized Words']
 list_test2 = [123, 8897, 42, 1168, 8675309]
 list_test3 = ['hello', 'world', 123, 'orange']
+
+
+# fun_list = ['hello', 'world', 123, 'orange']
+
+# fun_list.reverse()
+# print(fun_list)
